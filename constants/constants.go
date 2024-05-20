df52a5f79bb593a9e49369c8fe3ed09ff4c907e1
preetikansal2404 <preeti.kansal@sixt.com>
2024-05-20 11:36:21 +0530

    Removed secret

--- constants/constants.go
+++ constants/constants.go
@@ -1,8 +1,6 @@
 package constants
 
 const (
-	// TwilioAccountId = "AC4b4ceff57ea26376c5fef6227682e737"
-	// TwilioAuthenticationId      = "32bced1c1b9486021bc8d3fa7730f13c"
 	PhoneNumberSearchStr = "phone_no = ?"
 )
 
